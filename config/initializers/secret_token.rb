259eddab5f822a95679399f25f824ca70b4429ae
Jacob O'Donnell <jacobodonnell@gmail.com>
2013-03-02 16:33:35 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RefactorIt::Application.config.secret_token = '66465fac124d3c2159141988b249e20aa2fe562f39d09b24abf2321faa0e5fe953740f03d931557b6923b4fc0a3967d2761feb4b8a418ba0295233953c589a6e'
