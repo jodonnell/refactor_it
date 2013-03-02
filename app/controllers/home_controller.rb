dd691e07bdf3f0348e01459d6af3cab288234be4
Jacob O'Donnell <jacobodonnell@gmail.com>
2013-03-02 17:11:21 -0500

    added the first couple screens

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,17 @@ class HomeController < ApplicationController
   def index
     
   end
+
+  def login
+    email = params[:email]
+    session[:email] = email
+    redirect_to show_original_code_path
+  end
+
+  def show_original_code
+    
+  end
+
+  def submit_refactored_code
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 RefactorIt::Application.routes.draw do
-root :to => 'home#index'
+  root :to => 'home#index'
+  match 'login' => 'home#login'
+  match 'show_original_code' => 'home#show_original_code'
+  match 'submit_refactored_code' => 'home#submit_refactored_code'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
