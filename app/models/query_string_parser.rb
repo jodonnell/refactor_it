6ee77db0e5aef65adb4d25a37711632b86cb71b0
Jacob O'Donnell <jacobodonnell@gmail.com>
2013-03-02 21:55:54 -0500

    also decode keys

--- app/models/query_string_parser.rb
+++ app/models/query_string_parser.rb
@@ -6,7 +6,7 @@ class QueryStringParser
       q_str = url[a+1..url.length]
       q_array = q_str.split('&')
       @hash = {}
-      q_array.each {|part| parts = part.split('=', 2); @hash[parts[0].to_sym] = parts[1]}
+      q_array.each {|part| parts = part.split('=', 2); @hash[parts[0]] = parts[1]}
     else
       @hash = {}
     end
@@ -18,6 +18,11 @@ class QueryStringParser
       if value
         results[key] = value.gsub('+', ' ')
         results[key] = results[key].gsub('%26', '&')
+
+        if key.index '+' or key.index '%26'
+          results[key.gsub('+', ' ').gsub('%26', '&')] = results[key]
+          results.delete key
+        end
       else
         results[key] = value
       end
--- spec/models/query_string_parser_spec.rb
+++ spec/models/query_string_parser_spec.rb
@@ -2,11 +2,11 @@ require "spec_helper"
 
 describe QueryStringParser do
   it "can get a single pair of arguments" do
-    expect(QueryStringParser.new('http://www.example.com?baby=boomba').queryHash).to eq({baby: 'boomba'})
+    expect(QueryStringParser.new('http://www.example.com?baby=boomba').queryHash).to eq({'baby' => 'boomba'})
   end
 
   it "can get a multiple arguments" do
-    expect(QueryStringParser.new('http://www.example.com?baby=boomba&cookie=sugar').queryHash).to eq({baby: 'boomba', cookie: 'sugar'})
+    expect(QueryStringParser.new('http://www.example.com?baby=boomba&cookie=sugar').queryHash).to eq({'baby' => 'boomba', 'cookie' => 'sugar'})
   end
 
   it "handle no query string" do
@@ -14,19 +14,23 @@ describe QueryStringParser do
   end
 
   it "can handle two = signs without a seperating &" do
-    expect(QueryStringParser.new('http://www.example.com?poop=pam=pamper').queryHash).to eq({poop: 'pam=pamper'})
+    expect(QueryStringParser.new('http://www.example.com?poop=pam=pamper').queryHash).to eq({'poop' => 'pam=pamper'})
   end
 
   it "can handle no argument queries" do
-    expect(QueryStringParser.new('http://www.example.com?poop&pam=power&ping').queryHash).to eq({poop: nil, pam: "power", ping: nil})
+    expect(QueryStringParser.new('http://www.example.com?poop&pam=power&ping').queryHash).to eq({'poop' => nil, 'pam' => "power", 'ping' => nil})
   end
 
   it "can turn a + into a space" do
-    expect(QueryStringParser.new('http://www.example.com?value=boom+bam').queryHash).to eq({value: 'boom bam'})
+    expect(QueryStringParser.new('http://www.example.com?value=boom+bam').queryHash).to eq({'value' => 'boom bam'})
   end
 
   it "can decode %26" do
-    expect(QueryStringParser.new('http://www.example.com?value=boom+%26+bam').queryHash).to eq({value: 'boom & bam'})
+    expect(QueryStringParser.new('http://www.example.com?value=boom+%26+bam').queryHash).to eq({'value' => 'boom & bam'})
+  end
+
+  it "can decode keys as well" do
+    expect(QueryStringParser.new('http://www.example.com?boom+%26+bam=value').queryHash).to eq({'boom & bam' => 'value'})
   end
 
 
