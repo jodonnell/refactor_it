f2738f60a4fbe83c9f436d4e73d15c7cf3d74406
Jacob O'Donnell <jacobodonnell@gmail.com>
2013-03-12 00:11:19 -0400

    finally got clojure working, it's pretty ugly

--- code/cloj/src/cloj/core.clj
+++ code/cloj/src/cloj/core.clj
@@ -21,9 +21,21 @@
 (defn drop-empty-key [things]
   (filter #(not= "" (first %)) things))
 
+
+(defn merge-lists [& maps]
+  (reduce (fn [m1 m2]
+            (reduce (fn [m [k v]]
+                      (update-in m [k] (fnil conj []) v))
+                    m1, m2))
+          {}
+          maps))
+
+(defn get-hash [url]
+  (merge-lists (drop-empty-key (map split-on-equal (get-parts url)))))
+
 (defn query-hash [url]
   (if (= 2 (count (query-part url)))
-    (into {} (drop-empty-key (map split-on-equal (get-parts url))))
+    (zipmap (keys (get-hash url)) (map #(if (= 1 (count %)) (first %) %  ) (vals (get-hash url))))
     {}))
 
 
